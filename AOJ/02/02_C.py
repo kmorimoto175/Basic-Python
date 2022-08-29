093f0c1e2321d4b743544cb6a5b961b02ba000ff
kmorimoto175 <koheim21478963@gamil.com>
2022-08-29 13:33:38 +0900

    課題２修正”
    課題２修正
    
    git commit -m課題２修正”

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,10 +1,4 @@
-a, b, c = map(int, input().split())
-d = min(a,b,c)
-e = max(a,b,c)
-if d < a and a < e:
- f = a
-elif d < b and b < e:
- f = b
-else :
- f = c
-print(str(d) + " "+ str(f) + " " + str(e))
+a, b, c=map(int, input().split())
+listA=[a,b,c]
+listA.sort()
+print(listA[0], listA[1], listA[2])
